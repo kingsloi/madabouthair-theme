fca2a94a691042c132cd36f3b008600dff251910
Kingsley Raspe <kingsley.raspe@googlemail.com>
2017-04-03 23:13:26 -0500

    updated favicons

--- resources/views/frontend/shared/partials/meta.blade.php
+++ resources/views/frontend/shared/partials/meta.blade.php
@@ -12,11 +12,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
         <!-- Favicon -->
-        <link rel="shortcut icon" href="{{ asset('vendor/canvas/favicon.ico') }}">
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('vendor/canvas/apple-icon-144x144.png') }}">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('vendor/canvas/apple-icon-114x114.png') }}">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('vendor/canvas/apple-icon-72x72.png') }}">
-        <link rel="apple-touch-icon-precomposed" href="{{ asset('vendor/canvas/apple-icon-57x57.png') }}">
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('vendor/canvas/apple-touch-icon-57x57.png') }}" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('vendor/canvas/apple-touch-icon-72x72.png') }}" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('vendor/canvas/apple-touch-icon-144x144.png') }}" />
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ asset('vendor/canvas/apple-touch-icon-152x152.png') }}" />
+        <link rel="icon" type="image/png" href="{{ asset('vendor/canvas/favicon-32x32.png') }}" sizes="32x32" />
+        <link rel="icon" type="image/png" href="{{ asset('vendor/canvas/favicon-16x16.png') }}" sizes="16x16" />
+        <meta name="application-name" content="NWI-Based Hair Stylist Maddie Raspe"/>
+        <meta name="msapplication-TileColor" content="#C58C71" />
+        <meta name="msapplication-TileImage" content="{{ asset('vendor/canvas/mstile-144x144.png') }}" />
 
         <!-- Facebook Open Graph Tags -->
         <meta property="og:title" content="@yield('og-title')">
