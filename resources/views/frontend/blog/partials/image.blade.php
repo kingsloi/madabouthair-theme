3857c88a9265fd46d9b46ef45382be1530fca86f
Kingsley Raspe <kingsley.raspe@googlemail.com>
2017-04-12 10:25:51 -0500

    cheeky image wrap

--- resources/views/frontend/blog/partials/image.blade.php
+++ resources/views/frontend/blog/partials/image.blade.php
@@ -1,9 +1,13 @@
-<div class="c-gallery-list__item js-gallery__item">
-    <div class="c-gallery-item">
-        <a href="{{ $image['src'] }}" itemprop="contentUrl" data-size="{{ $image['width'] }}x{{ $image['height'] }}" class="c-gallery-item__inner js-gallery__link">
-            <figure class="o-image">
-                <img class="o-image__media" src="{{ $image['src'] }}" alt="{{ $image['alt'] }}" />
-            </figure>
-        </a>
+@if(!isset($hideTopLevelDiv))
+    <div class="c-gallery-list__item js-gallery__item">
+@endif
+        <div class="c-gallery-item">
+            <a href="{{ $image['src'] }}" itemprop="contentUrl" data-size="{{ $image['width'] }}x{{ $image['height'] }}" class="c-gallery-item__inner js-gallery__link">
+                <figure class="o-image">
+                    <img class="o-image__media" src="{{ $image['src'] }}" alt="{{ $image['alt'] }}" />
+                </figure>
+            </a>
+        </div>
+@if(!isset($hideTopLevelDiv))
     </div>
-</div>
+@endif
